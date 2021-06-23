f4f541bc0ae87fc2ee827551593bfdd9de188b9c
SergueiNK <nikolaev.serguei777@gmail.com>
2021-06-23 10:05:08 +0200

    add jquery to ajax request ok, need to see why it's work with POST and not with GET

--- papybot/static/scripts.js
+++ papybot/static/scripts.js
@@ -3,29 +3,45 @@
 //captur of event for JS
 //gonna surch the formular on JS
 //gonna surch the formular on JS
-let form = document.querySelector("#user-text-form");
+//let form = document.querySelector("#user-text-form");
 
-function postFormData (url, data){
-    return fetch(url, {
-        method: "POST",
-        body: data
-    })
+//function postFormData (url, data){
+    //return fetch(url, {
+        //method: "POST",
+        //body: data
+    //})
     // Recevoir le texte côté js. Concepte promesse. Fct va renvoyer un objet ce qui nous permet de réagir en cas de succés ou de l'erreur.
     // 1 Réagir en cas de succées
     // response => response.json() permet de transformer le texte en quelque chose qui rassemble à un objet js
-    .then(response => response.json())
+    //.then(paragraph_response => paragraph_response.json())
     // si il y a une erreur ça me permet de renvoyer l'erreur
-    .catch(error => console.log(error));
-}
+    //.catch(error => console.log(error));
+//}
 //listen the event of submission
-form.addEventListener("submit", function (event) {
+//form.addEventListener("submit", function (event) {
     //eviter que le comportement par default soitmise en route et d'eviter d'envoyer les données du seveur vers l'exterieur
-    event.preventDefault();
+    //event.preventDefault();
 
     // Envoyer le contenu du fomulaire au serveur
-    postFormData("/ajax", new FormData(form))
+    //postFormData("/ajax", new FormData(form))
     // Réagir en cas de succées dans .then je peux rahouter la publication du texte sur la page web
-    .then(response => {
-        console.log(response);
-    })
-})
+    //.then(paragraph_response => {
+        //console.log(paragraph_response);
+    //})
+//})
+
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+      $.ajax({
+         data : {
+            userText : $('#userText').val(),
+                },
+            type : 'POST',
+            url : '/ajax'
+           })
+       .done(function(data) {
+         $('#wiki_paragraph').text(data.output).show();
+     });
+     event.preventDefault();
+     });
+});
